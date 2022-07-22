79bb1994438c852d89453e148917c4e20021438a
Matheus <stvgamer74@gmail.com>
2022-07-22 18:02:50 -0300

    add linha em index.js

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
+
 app.listen(port, () => console.log(`Online: ${port}`));
